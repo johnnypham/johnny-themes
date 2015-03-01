2480019b9fcd0478fb7209db5b537ba36a166076
vienpn <vienpn@designveloper.com>
2015-03-01 20:57:18 +0700

    Create new page home with menu

--- template-home-connect.php
+++ template-home-connect.php
@@ -11,52 +11,79 @@
  * @subpackage Johnny_Themes
  * @since Johnny Themes 1.0
  */?>
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-	<head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-		<meta name="viewport" content="width=device-width, initial-scale=1"> 
-		<title>Home | Johnny Pham</title>
-		<meta name="description" content="Vien Pham profile,Johnny, html, css, html5, css3, johnny pro," />
-		<meta name="keywords" content="background , boxes, background image, css, tutorial" />		<meta name="author" content="Codrops" />
-		<link rel="shortcut icon" href="../favicon.ico">
+ <html class="no-js">
+ 	<head>
 		<link href='http://fonts.googleapis.com/css?family=Raleway:200,400,600' rel='stylesheet' type='text/css'>
+		<!--Menu-->
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/menu-primary/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/menu-primary/css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/menu-primary/fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/menu-primary/css/menu_bubble.css" />
+		<script src="<?php bloginfo('template_directory'); ?>/menu-primary/js/snap.svg-min.js"></script>
 
+
+		<!--[if IE]>
+  		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<!--Menu-->	
 
 		
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/template-home/css/normalize.css" />
-		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/template-home/csss/demo.css" />
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/template-home/css/component.css" />
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/template-home/css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/animate.css">
 		<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/cus-animation.css">
 
-	
-
-	</head>
-	<body>
+			<head>
+				<body>
 		<div class="container demo-1">
 			<div class="content">
 				<div id="large-header" class="large-header">
 					<canvas id="demo-canvas"></canvas>
 					<div class="main-title">
 						
-					<h1 class="animation-step-1 animated  zoomInDown"  style="font-size:55px" class="">Profile</h1>
-					<img class="animation-step-5 animated   fadeInUp" style="border-radius: 100%;" width='200' height='200' src="<?php bloginfo('template_directory'); ?>/img/johnny-pham.jpg" alt="Image 1"/>
-
+					<h1 class="animation-step-1 animated  zoomInDown thin"  style="font-size:55px" class="">Profile</h1>
+						<a id="circle" class="ec-circle animation-step-2 animated  fadeInUp" href="#">
+								
+						</a>
 					</div>
 					
 				</div>
-				
 
+			
+			
 			</div>
 			<!-- Related demos -->
-		</div><!-- /container -->
-	
 
-		<script src="<?php bloginfo('template_directory'); ?>/template-home/js/TweenLite.min.js"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/template-home/js/EasePack.min.js"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/template-home/js/rAF.js"></script>
-		<script src="<?php bloginfo('template_directory'); ?>/template-home/js/demo-1.js"></script>
-	</body> 
+		</div><!-- /container -->
+		<div class="container">
+			<div class="menu-wrap">
+				<nav class="menu">
+					<div class="icon-list">
+						<a href="#"><i class="fa fa-home"></i><span>Home</span></a>
+						<a href="#"><i class="fa fa-archive"></i><span>Profile</span></a>
+						<a href="#"><i class="fa fa-file-o"></i><span>Resume</span></a>
+						<a href="#"><i class="fa fa-newspaper-o"></i><span>News</span></a>
+						<a href="#"><i class="fa fa-inbox fa-envelope-o"></i><span>Contact</span></a>
+						<a href="#"><i class="fa fa-fw fa-bar-chart-o"></i><span>Analytics</span></a>
+
+						<a href="#"><i class="fa fa-fw fa-newspaper-o"></i><span>Reading List</span></a>
+					</div>
+				</nav>
+				<button class="close-button" id="close-button">Close Menu</button>
+				<div class="morph-shape" id="morph-shape" data-morph-open="M-7.312,0H15c0,0,66,113.339,66,399.5C81,664.006,15,800,15,800H-7.312V0z;M-7.312,0H100c0,0,0,113.839,0,400c0,264.506,0,400,0,400H-7.312V0z">
+					<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 100 800" preserveAspectRatio="none">
+						<path d="M-7.312,0H0c0,0,0,113.839,0,400c0,264.506,0,400,0,400h-7.312V0z"/>
+					</svg>
+				</div>
+			</div>	
+			<button class="menu-button" id="open-button">Open Menu</button>				
+		</div>
+</body>
+<script src="<?php bloginfo('template_directory'); ?>/template-home/js/TweenLite.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/template-home/js/EasePack.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/template-home/js/rAF.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/template-home/js/demo-1.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/menu-primary/js/classie.js"></script>
+		<script src="<?php bloginfo('template_directory'); ?>/menu-primary/js/main4.js"></script>
 </html>
